686cfad357d11bee3fab639fa30dd43c3b77e172
Oleg Kokhtenko <kohtenko@gmail.com>
2017-01-28 10:28:07 +0100

    cleanup and structure

---
+++ GreenChain/DeviceCell.swift
@@ -0,0 +1,24 @@
+//
+//  DeviceCell.swift
+//  GreenChain
+//
+//  Created by Oleg Kokhtenko on 28/01/2017.
+//  Copyright © 2017 DataArt. All rights reserved.
+//
+
+import UIKit
+
+class DeviceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GreenChain/DeviceListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DeviceListViewController.swift
+//  GreenChain
+//
+//  Created by Oleg Kokhtenko on 28/01/2017.
+//  Copyright © 2017 DataArt. All rights reserved.
+//
+
+import UIKit
+
+class DeviceListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "device", for: indexPath)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        self.performSegue(withIdentifier: "push", sender: indexPath)
+    }
+
+}
